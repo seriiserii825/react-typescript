acbeaeda132c15183cd08d1fc881ab724a2ee542
serii <seriiburduja@gmail.com>
2023-05-01 20:31:40 +0300

    ReadMore

---
+++ src/components/projects/our-tours/OurToursItem.tsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import {ITour} from "../../../interface/ITour";
+
+function OurToursItem({tour}: { tour: ITour }) {
+    const [read_more, setReadMore] = useState<boolean>(true);
+    return (
+        <article className={'our-tours__item'}>
+            <div className={'our-tours__img'}>
+                <img src={tour.image} alt=""/>
+            </div>
+            <header className="our-tours__header">
+                <h2 className="our-tours__title">{tour.name}</h2>
+                <div className="our-tours__price">${tour.price}</div>
+            </header>
+            <div className="our-tours__content">
+                {read_more ? tour.info.substring(0, 200) : tour.info}
+                <button className={'our-tours__read-more'} onClick={() => setReadMore(!read_more)}>{read_more ? 'Read' +
+                    ' More' : 'Show' +
+                    ' Less'}</button>
+            </div>
+            <button className="btn">Not interested</button>
+        </article>
+    );
+}
+
+export default OurToursItem;
--- src/views/OurToursView.tsx
+++ src/views/OurToursView.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {ITour} from "../interface/ITour";
 import axiosInstance from "../helpers/axios-instance";
 import Loading from "../components/globals/Loading";
+import OurToursItem from "../components/projects/our-tours/OurToursItem";
 
 function OurToursView() {
     const [loading, setLoading] = useState<boolean>(true);
@@ -29,17 +30,7 @@ function OurToursView() {
             {loading ? <Loading/> : (
                 <div className={'our-tours__body'}>
                     {tours.length > 0 ? tours.map((tour: ITour) => (
-                        <article className={'our-tours__item'} key={tour.id}>
-                            <div className={'our-tours__img'}>
-                                <img src={tour.image} alt=""/>
-                            </div>
-                            <header className="our-tours__header">
-                                <h2 className="our-tours__title">{tour.name}</h2>
-                                <div className="our-tours__price">{tour.price}</div>
-                            </header>
-                            <div className="our-tours__content">{tour.info}</div>
-                            <button className="btn">Not interested</button>
-                        </article>
+                        <OurToursItem tour={tour} key={tour.id}/>
                     )) : (<h2>No tours available...</h2>)
                     }
                 </div>
